f1ad982b736e96a7e7038f35d68dbe460ec5bfb9
Carlos Amorim <carlos.av.amorim@gmail.com>
2021-10-13 22:34:49 -0300

    Adicionando itens na lista

--- Test_B7Web/src/App.js
+++ Test_B7Web/src/App.js
@@ -5,37 +5,47 @@ import './App.css';
 
 function App() {
 
-  const [texto, setTexto] = React.useState('')
+  const [texto, setTexto] = React.useState('')      //Altera o texto do app, com base na props passada de outro componente
   const [list, setList] = React.useState([])
 
-  function handleSearchInput(texto) {
+  function handleSearchInput(texto) {             //Altera o texto do app, com base na props passada de outro componente
     setTexto(texto)
   }
 
   React.useEffect(()=>{
     setList([
-      {nome: 'Carlos', numero: 14998057785},
-      {nome: 'Danielle', numero: 149985674},
-      {nome: 'Sophia', numero: 148579642},
+      {title: 'Terapia', done: true},
+      {title: 'Ligar para o médico', done: false},
+      {title: 'Comprar um bolo', done: false},
     ])
   },[])
+
+  function addItem(novoItem) {
+    /*alert(novoItem)*/                                           // Verificando se foi recebido o value de outro componente
+    const newList = [...list, {title: novoItem, done: false}]
+    setList(newList)                                               // Adicionando a newList na list
+  }
 
   return (<>
     <h1>Lista de tarefas</h1>
     <SearchBox 
-    frasePadrao='Busque algo'
-    onchangeText={handleSearchInput}
+    frasePadrao='Adicione um item'
+    onchangeText={handleSearchInput}          //Altera o texto do app, com base na props passada de outro componente, função recebe o valor do outro componente
+    pressEnter={addItem}                      //Coletando o value digitado após apertar Enter, value veio de outro componente
     />
 
     <hr/>
 
-    <p>Texto procurado é {texto} </p>
+    <p>Texto procurado é {texto}</p>
 
     <ul>
-      {list.map((item, key)=>(
+      {list.map((item, key)=>(                //Listar itens de list
         <li key={key}>
-          {item.nome} - {item.numero}
-        </li>
+          {item.done &&                       //Se done for true a tag <del> irá passar um risco, se não, escrever normal
+          <del>{item.title}</del>
+        }
+        {!item.done &&
+          item.title}</li>
       ))}
     </ul>
 
--- Test_B7Web/src/components/SearchBox.js
+++ Test_B7Web/src/components/SearchBox.js
@@ -4,19 +4,28 @@ export default function SearchBox(props) {
 
     const [texto, setTexto] = React.useState('')
 
-  function inputTextChange(e) {
+  function inputTextChange(e) {               // Validando o value
     setTexto(e.target.value)
   }
 
-  React.useEffect(()=>(
-    props.onchangeText(texto)
+  React.useEffect(()=>(                       // Adicionando o texto na Props, para transferir o value para outro componente
+    props.onchangeText(texto)                 // Props.onchangeText recebe o value a cada mudança no value e transfere para outro componente
   ), [texto])
+
+  function collectKey(e) {
+    /*console.log(e.keyCode)*/
+    if (e.keyCode == 13){                     // Verificando se tecla digitada é Enter
+      props.pressEnter(texto)                 // Passando o texto digitado para a props
+      setTexto('')                            // Limpando texto digitado
+    }
+  }
 
     return (
         <div>
             <input type='text' 
             value={texto}
             onChange={inputTextChange}
+            onKeyUp={collectKey}
             placeholder={props.frasePadrao ?? "Digite alguma coisa"}>
 
             </input>
