2e715a38359510383ba238ad27e086c610ecc490
Carlos <carlos.av.amorim@gmail.com>
2021-10-19 18:22:17 -0300

    Forms atualizados

--- Tela de Login/src/components/body/Body.js
+++ Tela de Login/src/components/body/Body.js
@@ -4,33 +4,29 @@ import perfil from '../../img/perfil.png'
 import facebook from '../../img/login/face.png'
 import google from '../../img/login/google.png'
 import linkedin from '../../img/login/linkedin.png'
+//import { useFormik } from 'formik';
 
 
-export default function Body(props) {
+export default function Body() {
 
-    const [texto, setTexto] = React.useState('')
-
-    function handleText(e) {
-        setTexto(e.target.value)
-    }
-    
-    // Remover as divs e encapsular os formulários em um Form.
 
     return (
-        <div className="flex items-stretch my-5 mx-auto container w-full h-3/5 bg-center">
+        <div className="flex items-stretch my-5 mx-auto container w-full h-3/5 bg-center"> 
             <div className="flex-col w-2/4 my-auto mx-auto drop-shadow-2xl rounded-3xl bg-opacity-25 bg-white">
-                <div className="w-20 my-4 mx-auto">
-                    <img src={perfil} className="h-full w-full"/>
+                <div className="w-20 my-4 mx-auto"> 
+                    <img src={perfil} alt="" className="h-full w-full"/>
                 </div>
-                <div className="flex p-4 my-0 mx-auto ">
-                    <input type="email" placeholder="Digite seu e-mail" onChange={handleText} value={texto} className="login py-2 bg-white mx-auto w-full rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"></input>
-                </div>
-                <div className="flex p-4 mx-auto ">
-                    <input type="password" placeholder="Digite sua senha" className="senha py-2 bg-white mx-auto w-full rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"></input>
-                </div>
-                <div className="flex">
-                    <button className="border-2 mx-auto rounded-xl my-4 px-5 py-2 w-5/6 text-white bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 hover:bg-pink-500">Login!</button>
-                </div>
+                    <form>
+                        <label className="flex p-4 my-0 mx-auto ">
+                            <input type="email" placeholder="Digite seu e-mail" className="login py-2 bg-white mx-auto w-full rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"></input>
+                        </label>
+                        <label className="flex p-4 mx-auto ">
+                            <input type="password" placeholder="Digite sua senha" className="senha py-2 bg-white mx-auto w-full rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:border-transparent"></input>
+                        </label>
+                        <div className="flex">
+                            <button className="border-2 mx-auto rounded-xl my-4 px-5 py-2 w-5/6 text-white bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 hover:bg-pink-500">Login!</button>
+                        </div>
+                    </form>
                 <hr/>
                 <div className="mx-auto my-5">
                     <div className="my-4 flex justify-center">
@@ -46,11 +42,11 @@ export default function Body(props) {
                         </div>
                         <div>
                             <img src={linkedin} />
-                        </div>
+                        </div> 
                     </div>
                     
                 </div>
             </div>
         </div>
     )
-}
+} // eslint-disable-next-line
