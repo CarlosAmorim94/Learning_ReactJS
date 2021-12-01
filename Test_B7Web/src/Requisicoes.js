6b3ae3eb862d02ce6c77e5d4a426d80d113390e6
Carlos <carlos.av.amorim@gmail.com>
2021-11-30 22:29:05 -0300

    Adicionando loading

--- Test_B7Web/src/Requisicoes.js
+++ Test_B7Web/src/Requisicoes.js
@@ -3,12 +3,15 @@ import React from "react"
 const Requisicoes = () => {
 
     const [cars, setCars] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
 
     function getCars() {
+        setLoading(true)
         fetch("https://api.b7web.com.br/carros/api/carros")
             .then(function(result) {
                 return result.json()
             }).then(function(json) {
+                setLoading(false)
                 setCars(json.cars)
             })
     }
@@ -21,12 +24,13 @@ const Requisicoes = () => {
             <h1>Testando APIs - Carros</h1>
             <button onClick={getCars}>Carregar carros</button>
             <div>
-            {cars.map((item, key)=>(
-                <div key={key}>
-                    <img src={item.photo} width="200" alt="teste" />
-                    <h3>{item.brand} - {item.name}</h3>
-                </div>
-            ))}
+                {loading === true && <h1>Carregando</h1>}
+                {cars.map((item, key)=>(
+                    <div key={key}>
+                        <img src={item.photo} width="200" alt="teste" />
+                        <h3>{item.brand} - {item.name}</h3>
+                    </div>
+                ))}
             </div>
         </>
     )
