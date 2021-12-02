02fe7500552c21b633d696c63ab2879b3b4e52f9
Carlos <carlos.av.amorim@gmail.com>
2021-12-01 22:05:12 -0300

    Adicionando POST

--- Test_B7Web/src/Requisicoes.js
+++ Test_B7Web/src/Requisicoes.js
@@ -5,15 +5,29 @@ const Requisicoes = () => {
     const [cars, setCars] = React.useState([])              // Adicionar API convertida para json num array para exibir
     const [loading, setLoading] = React.useState(false)     // informação de "Carregando".
     const [year, setYear] = React.useState('')              // Filtro para o ano
+    const [emailField, setEmailField] = React.useState('')  // Coletar email para POST
+    const [passwordField, setPasswordField] = React.useState('')  // Coletar senha para POST
 
-    function getCars() {
+    const getCars = async () => {
+    // async function getCars() {...} Outro modo de fazer função assincrona
         setCars([])
         setLoading(true)
         
-        /* const url = "https://api.b7web.com.br/carros/api/carros"
-            if (url != '') {
-                url += '?ano='+year
-        }   */                                              //Opções para o filtro, adicionar variavel url ao fetch(url)
+
+        let result = await fetch(`https://api.b7web.com.br/carros/api/carros?ano=${year}`)   // coleta informações
+        let json = await result.json()                                                       // converte as informações em JSON 
+            if (json.error === '') {                                                         // Adiciona a uma lista
+                setCars(json.cars)
+            } else {
+                alert(json.error)
+            }
+    }
+
+    /* ------------ OUTRA OPÇÃO DE FUNÇÃO FETCH ----------
+    const getCars = () => {                            
+        setCars([])
+        setLoading(true)
+        
 
         fetch(`https://api.b7web.com.br/carros/api/carros?ano=${year}`)
             .then(function(result) {
@@ -29,6 +43,7 @@ const Requisicoes = () => {
                 
             })
     }
+    */
 
     React.useEffect(()=>{
         getCars()
@@ -39,7 +54,49 @@ const Requisicoes = () => {
         //getCars() não funciona direito, aplicar no Effect para monitorar essa função
     }
 
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault()
+
+        let url = 'https://api.b7web.com.br/carros/api/auth/login'
+
+        let result = await fetch(url,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: emailField,
+                password: passwordField
+            })
+        })
+        let json = await result.json()
+            if(json.error !== '') {
+                alert(json.error)
+            }
+
+            console.log("RESULT", json)
+    }
+
     return(<>
+
+            <h2>Faça login</h2>
+            <form onSubmit={handleLoginSubmit}>
+                <label>Email:
+                    <input 
+                    type="email" 
+                    value={emailField}
+                    onChange={(e)=>(setEmailField(e.target.value))}></input>
+                </label><br/>
+                <label>Senha:
+                    <input 
+                    type="password" 
+                    value={passwordField}
+                    onChange={(e)=>(setPasswordField(e.target.value))}></input>
+                </label><br/>
+                <button type="submit">Enviar</button>
+            </form>
+
+            <hr/>
             <h1>Testando APIs - Carros</h1>
             <select onChange={handleFilter}>
                 <option></option>
